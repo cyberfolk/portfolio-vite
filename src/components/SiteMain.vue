756bbe17ebf0e34c561cd26c4485065abdd999c9
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-13 17:02:06 +0200

    Use component to show Projects

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,12 +1,26 @@
 <script>
+import { state } from "../state.js";
+import ProjectsList from "./section/ProjectsList.vue";
 export default {
   name: "SiteMain",
-  components: {},
+  components: {
+    ProjectsList,
+  },
+  data() {
+    return { state, };
+  },
+  mounted() {
+    state.getProjects();
+  },
 };
+
 </script>
 
 <template>
-  <main></main>
+  <main id="app_main" class="m-5">
+    <ProjectsList :projects="state.projects" />
+  </main>
+  <!-- /#app_main -->
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/item/ProjectItem.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+    name: "ProjectItem",
+    props: {
+        title: String,
+        cover: String,
+        description: String,
+    },
+
+};
+</script>
+<template>
+    <div class="card position-relative">
+        <img v-if="cover" :src="cover" class="card-img" :alt="title" />
+        <div class="card-body h-100 w-100 text-center">
+            <h5 class="card-title mb-0">{{ title }}</h5>
+            <!-- /.ms_title -->
+            <p class="card-text ms_description">{{ description }}</p>
+            <!-- /.ms_description -->
+        </div>
+        <!-- /.body -->
+    </div>
+    <!-- /.card -->
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/app.scss';
+
+.card {
+    position: relative;
+    aspect-ratio: 0.66;
+
+    .card-img {
+        position: relative;
+        z-index: 2;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .card-body {
+        position: absolute;
+        z-index: 1;
+        overflow-y: auto;
+
+        /*         color: $light;
+ */
+        /* width */
+        &::-webkit-scrollbar {
+            width: 20px;
+        }
+
+        /* Handle */
+        &::-webkit-scrollbar-thumb {
+            border-radius: 40px;
+            box-shadow: inset 0 0 20px 20px black;
+            border: solid 6px transparent;
+        }
+
+
+        .ms_description {
+            font-style: italic;
+            font-size: 0.8rem;
+            text-align: justify
+        }
+    }
+}
+
+.card:hover .card-img {
+    filter: brightness(0.2);
+}
+
+.card:hover .card-body {
+    z-index: 2;
+}
+</style>
