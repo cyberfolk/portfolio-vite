880ec30191a6197fa92096ad46c81017b6a50b3a
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-13 17:00:58 +0200

    Implements getProjects()

--- src/state.js
+++ src/state.js
@@ -1,4 +1,23 @@
 import { reactive } from 'vue'
 import axios from "axios";
 
-export const state = reactive({});
+export const state = reactive({
+    loadingProjects: true,
+    API_URL_BASE: "http://127.0.0.1:8000/",
+    API_URL_PROJECTS: "api/projects",
+    projects: [],
+
+    getProjects() {
+        const url = this.API_URL_BASE + this.API_URL_PROJECTS;
+        axios
+            .get(url)
+            .then(response => {
+                this.projects = response.data.projects;
+                this.loadingProjects = false;
+                console.log(this.projects);
+            })
+            .catch(error => {
+                this.error = error.message
+            })
+    }
+});
