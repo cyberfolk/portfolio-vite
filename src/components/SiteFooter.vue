3543d6c455af60ff739615247080fea99eb1c674
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-13 17:01:20 +0200

    Add personal tag

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -6,7 +6,12 @@ export default {
 </script>
 
 <template>
-  <footer></footer>
+  <footer id="app_footer" class="text-center py-3 bg-secondary">
+    <div class="container">
+      <span class="text-center text-light"> Made with love &copy; powered by cyberfolk</span>
+    </div>
+  </footer>
+  <!-- /#app_footer -->
 </template>
 
 <style lang="scss" scoped>
