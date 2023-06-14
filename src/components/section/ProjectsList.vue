ba4cc0feb32f2c57439b7dc0b53e4694d3650235
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-15 00:42:11 +0200

    Add pagination to projects

--- src/components/section/ProjectsList.vue
+++ src/components/section/ProjectsList.vue
@@ -8,10 +8,11 @@ export default {
         return { state, };
     },
     mounted() {
-        state.getProjects();
+        state.fetchProjects(state.API_URL_BASE + state.API_URL_PROJECTS + state.currentPage);
     },
 };
 </script>
+
 <template>
     <section id="projects_list">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
@@ -24,6 +25,15 @@ export default {
             <!-- /.col -->
         </div>
         <!-- /.row -->
+
+        <nav class="mt-3">
+            <ul class="pagination">
+                <li class="page-item" v-for="link in  state.links" :class="link.active ? 'active' : ''" @click="state.fetchProjects(link.url)">
+                    <a class="page-link" href="#" v-html="link.label"></a>
+                </li>
+            </ul>
+        </nav>
+
     </section>
     <!-- /#projects_list -->
 </template>
--- src/state.js
+++ src/state.js
@@ -5,19 +5,20 @@ export const state = reactive({
     loadingProjects: true,
     loadingProject: true,
     API_URL_BASE: "http://127.0.0.1:8000/",
-    API_URL_PROJECTS: "api/projects",
+    API_URL_PROJECTS: "api/projects?page=",
     API_URL_PROJECT: "api/projects",
     projects: [],
+    links: [], // Used to pagination
     project: null,
+    currentPage: 1,
 
-    getProjects() {
-        const url = this.API_URL_BASE + this.API_URL_PROJECTS;
+    fetchProjects(url) {
         axios
             .get(url)
             .then(response => {
                 this.projects = response.data.projects.data;
+                this.links = response.data.projects.links;
                 this.loadingProjects = false;
-                console.log(this.projects);
             })
             .catch(error => {
                 this.error = error.message
