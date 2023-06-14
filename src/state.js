8ee18415baff91469f9688ec2b23db27a1d707f7
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 14:41:03 +0200

    Add getProject(slug)

--- src/state.js
+++ src/state.js
@@ -3,9 +3,12 @@ import axios from "axios";
 
 export const state = reactive({
     loadingProjects: true,
+    loadingProject: true,
     API_URL_BASE: "http://127.0.0.1:8000/",
     API_URL_PROJECTS: "api/projects",
+    API_URL_PROJECT: "api/projects",
     projects: [],
+    project: null,
 
     getProjects() {
         const url = this.API_URL_BASE + this.API_URL_PROJECTS;
@@ -15,6 +18,22 @@ export const state = reactive({
                 this.projects = response.data.projects;
                 this.loadingProjects = false;
                 console.log(this.projects);
+            })
+            .catch(error => {
+                this.error = error.message
+            })
+    },
+
+    getProject(slug) {
+        const url = this.API_URL_BASE + this.API_URL_PROJECTS + "/" + slug;
+        axios
+            .get(url)
+            .then(response => {
+                if (response.data.succes) {
+                    this.project = response.data.result;
+                } else {
+
+                }
             })
             .catch(error => {
                 this.error = error.message
