26b16ed271756d5b81b2f7f2a21bc7e6c69f91d0
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 15:00:36 +0200

    Add state and ProjectsList

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <script>
+import { state } from "../state.js";
+import ProjectsList from "./../components/section/ProjectsList.vue";
 export default {
     name: "HomeView",
+    components: {
+        ProjectsList,
+    },
+    data() {
+        return { state, };
+    },
+    mounted() {
+        state.getProjects();
+    },
 };
 
 </script>
 
 <template>
-    <h1>Home View</h1>
+    <div id="view_home" class="m-5">
+        <h1>Home View</h1>
+        <ProjectsList :projects="state.projects" />
+    </div>
+    <!-- /#view_home -->
 </template>
 
 <style lang="scss" scoped>
