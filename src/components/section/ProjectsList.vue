a3690bf155aef328228e3ae31afa79ac1c1f2641
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 15:00:06 +0200

    Include templete in a section

--- src/components/section/ProjectsList.vue
+++ src/components/section/ProjectsList.vue
@@ -7,16 +7,19 @@ export default {
 };
 </script>
 <template>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
-        <div class="col" v-for="p in projects">
-            <ProjectItem
-                :title="p.title"
-                :cover="p.link_cover"
-                :overview="p.description" />
+    <section id="projects_list">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
+            <div class="col" v-for="p in projects">
+                <ProjectItem
+                    :title="p.title"
+                    :cover="p.link_cover"
+                    :overview="p.description" />
+            </div>
+            <!-- /.col -->
         </div>
-        <!-- /.col -->
-    </div>
-    <!-- /.row -->
+        <!-- /.row -->
+    </section>
+    <!-- /#projects_list -->
 </template>
 
 <style lang="scss" scoped>
