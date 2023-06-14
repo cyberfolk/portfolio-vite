23b51eb5b20f01f2393e8e5f375603bf9a5f2577
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 15:01:08 +0200

    Create templete

---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "SingleProjectView",
+};
+
+</script>
+
+<template>
+    <h1>SingleProject View</h1>
+    {{ $route.params.slug }}
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/app.scss';
+</style>
