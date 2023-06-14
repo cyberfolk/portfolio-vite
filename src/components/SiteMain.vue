53b446fc85bad5085dd24711c20a5f143101bd75
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 14:59:25 +0200

    Remove all components

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,25 +1,12 @@
 <script>
-import { state } from "../state.js";
-import ProjectsList from "./section/ProjectsList.vue";
 export default {
   name: "SiteMain",
-  components: {
-    ProjectsList,
-  },
-  data() {
-    return { state, };
-  },
-  mounted() {
-    state.getProjects();
-  },
 };
 
 </script>
 
 <template>
-  <main id="app_main" class="m-5">
-    <ProjectsList :projects="state.projects" />
-  </main>
+  <main id="app_main" class="m-5"></main>
   <!-- /#app_main -->
 </template>
 
