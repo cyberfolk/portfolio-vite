3d421ae83dc6a386778ac96ae9e6c423a057d29c
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 17:22:53 +0200

    Show card witrh img stored in backend

--- src/components/section/ProjectsList.vue
+++ src/components/section/ProjectsList.vue
@@ -1,18 +1,24 @@
 <script>
+import { state } from "../../state";
 import ProjectItem from "../item/ProjectItem.vue";
 export default {
     name: "ProjectsList",
-    components: { ProjectItem, },
-    props: { projects: Array, },
+    components: { ProjectItem, PaginationProjects },
+    data() {
+        return { state, };
+    },
+    mounted() {
+        state.getProjects();
+    },
 };
 </script>
 <template>
     <section id="projects_list">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
-            <div class="col" v-for="p in projects">
+            <div class="col" v-for="p in state.projects">
                 <ProjectItem
                     :title="p.title"
-                    :cover="p.link_cover"
+                    :cover="state.completePath(p)"
                     :overview="p.description" />
             </div>
             <!-- /.col -->
