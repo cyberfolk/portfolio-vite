07e6788fcd9989dd87a0aeb7cf1f92eca318219b
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-16 00:37:42 +0200

    Add form to contact

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -7,8 +7,37 @@ export default {
 
 <template>
     <div id="contacts_view">
-        <div class="container">
+        <div class="container my-5">
             <h1 class="my-5 f_skratch">Contacts View</h1>
+            <form class="mb-5 py-3">
+                <div class="mb-5">
+                    <label for="name" class="form-label">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class="form-control"
+                        placeholder="Type your full name here" />
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="form-label">Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        class="form-control"
+                        placeholder="Type your email here" />
+                </div>
+
+                <div class="mb-5">
+                    <div for="message" class="form-label">Message:</div>
+                    <textarea class="form-control rounded" name="message" id="message" rows="8"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary me-2">Send</button>
+                <button type="reset" class="btn btn-warning">Reset</button>
+            </form>
         </div>
         <!-- /.container -->
     </div>
