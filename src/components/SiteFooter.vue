18ff113017064d87345741b0f68802e8b584ac7d
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-16 00:35:46 +0200

    Add margin e padding

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -6,20 +6,23 @@ export default {
 </script>
 
 <template>
-  <footer id="app_footer" class="d-flex flex-wrap justify-content-between align-items-center p-5 py-3 bg-dark">
-    <div class="align-items-center text-light"> Andrea Ferrari | Boolean 2023 | &copy; powered by cyberfolk</div>
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex align-content-center gap-4">
-      <li>
-        <a href="https://t.me/Remigio377">
-          <font-awesome-icon icon="fa-brands fa-telegram" size="lg" />
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/cyberfolk">
-          <font-awesome-icon icon="fa-brands fa-github-alt" size="xl" />
-        </a>
-      </li>
-    </ul>
+  <footer id="app_footer" class="p-5 py-3 bg-dark fixed-bottom">
+    <div class="d-flex flex-wrap justify-content-between align-items-center">
+      <div> Andrea Ferrari | Boolean 2023 | &copy; powered by cyberfolk</div>
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex align-content-center gap-4">
+        <li>
+          <a href="https://t.me/Remigio377">
+            <font-awesome-icon icon="fa-brands fa-telegram" size="lg" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/cyberfolk">
+            <font-awesome-icon icon="fa-brands fa-github-alt" size="xl" />
+          </a>
+        </li>
+      </ul>
+    </div>
+
   </footer>
   <!-- /#app_footer -->
 </template>
