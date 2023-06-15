0c7f2a4e416b096eaf63a6cf26db55a4502f9a30
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-15 02:21:04 +0200

    Restyle and add props:type, slug

--- src/components/item/ProjectItem.vue
+++ src/components/item/ProjectItem.vue
@@ -5,21 +5,33 @@ export default {
         title: String,
         cover: String,
         description: String,
+        type: String,
+        slug: String,
     },
+    methods: {
+        getBG(type) {
+            switch (type) {
+                case 'full-stack': return 'bg-success';
+                case 'back-end': return 'bg-warning';
+                case 'front-end': return 'bg-danger';
+            }
+        }
+    }
 
 };
 </script>
 <template>
     <div class="card position-relative">
-        <img v-if="cover" :src="cover" class="card-img" :alt="title" />
-        <div class="card-body h-100 w-100 text-center">
-            <h5 class="card-title mb-0">{{ title }}</h5>
-            <!-- /.ms_title -->
-            <p class="card-text ms_description">{{ description }}</p>
-            <!-- /.ms_description -->
-            <!-- <router-link class="nav-link" :to="{ name: 'single-project', param: { 'slug': post.slug } }">Read More</router-link> -->
+        <img v-if="cover" :src="cover" class="card-img-top" :alt="title" />
+        <div class="badge position-absolute top-0 end-0 mt-1 me-1" :class="getBG(type.name)">{{ type.name }}</div>
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ description.slice(0, 150) + '...' }}</p>
         </div>
         <!-- /.body -->
+        <div class="card-footer text-primary text-center">
+            <router-link class="nav-link" :to="{ name: 'single-project', params: { slug: slug } }">Read More</router-link>
+        </div>
     </div>
     <!-- /.card -->
 </template>
@@ -28,49 +40,15 @@ export default {
 @use '../../assets/scss/app.scss';
 
 .card {
-    position: relative;
     aspect-ratio: 1.33;
 
     .card-img {
-        position: relative;
-        z-index: 2;
         height: 100%;
         object-fit: cover;
     }
 
     .card-body {
-        position: absolute;
-        z-index: 1;
-        overflow-y: auto;
-
-        /*         color: $light;
- */
-        /* width */
-        &::-webkit-scrollbar {
-            width: 20px;
-        }
-
-        /* Handle */
-        &::-webkit-scrollbar-thumb {
-            border-radius: 40px;
-            box-shadow: inset 0 0 20px 20px black;
-            border: solid 6px transparent;
-        }
-
-
-        .ms_description {
-            font-style: italic;
-            font-size: 0.8rem;
-            text-align: justify
-        }
+        height: 400px;
     }
-}
-
-.card:hover .card-img {
-    filter: brightness(0.2);
-}
-
-.card:hover .card-body {
-    z-index: 2;
 }
 </style>
