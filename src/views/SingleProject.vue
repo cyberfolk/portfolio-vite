01a772f402eda72c0f655770f6ffd122b715337b
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-15 09:27:05 +0200

    Rename in SingleProject

---
+++ src/views/SingleProject.vue
@@ -0,0 +1,25 @@
+
+<script>
+import { state } from "../state";
+export default {
+    name: "SingleProject",
+    data() {
+        return { state, };
+    },
+    mounted() {
+        state.getProject(this.$route.params.slug);
+    },
+};
+</script>
+
+<template>
+    <div class="container" v-if="state.project">
+        <h1>{{ state.project.title }}</h1>
+        <h3>SingleProject View</h3>
+        {{ this.$route.params.slug }}
+    </div>
+</template> 
+
+<style lang="scss" scoped>
+@use '../assets/scss/app.scss';
+</style>
