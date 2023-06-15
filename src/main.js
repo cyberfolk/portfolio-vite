f219a70e094d672c03e9b757df4f4aa19dfeab33
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-15 17:11:04 +0200

    Add icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/scss/app.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+
+import { createApp } from 'vue'
 import { router } from './router.js'
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faTelegram, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faUserSecret, faGithubAlt, faTelegram)
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
