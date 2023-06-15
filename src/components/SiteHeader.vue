9f2c306dcf8dea3c4d6c856b397a842765847149
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-15 12:30:12 +0200

    Add icon and restyle navbar

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -7,10 +7,27 @@ export default {
 
 <template>
   <header id="app_header">
-    <nav class="nav justify-content-center bg-dark shadow ">
-      <router-link class="nav-link" :to="{ 'name': 'home' }" aria-current="page">Home</router-link>
-      <router-link class="nav-link" :to="{ 'name': 'about' }" aria-current="page">About</router-link>
-      <router-link class="nav-link" :to="{ 'name': 'contacts' }" aria-current="page">Contacts</router-link>
+
+    <nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark shadow px-5">
+      <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+        <img height="40" src="./../assets/image/cyberfolk-logo.png" alt="Personal Logo">
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation'">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- navbar-toggler -->
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item"><router-link class="nav-link text-white" :to="{ 'name': 'home' }" aria-current="page">Home</router-link></li>
+          <li class="nav-item"><router-link class="nav-link text-white" :to="{ 'name': 'contacts' }" aria-current="page">Contacts</router-link></li>
+          <li class="nav-item"><router-link class="nav-link text-white" :to="{ 'name': 'projects' }" aria-current="page">Projects</router-link></li>
+          <li class="nav-item"><router-link class="nav-link text-white" :to="{ 'name': 'about' }" aria-current="page">About</router-link></li>
+        </ul>
+      </div>
+      <!-- navbar-collapse -->
     </nav>
   </header>
   <!-- /#app_header -->
