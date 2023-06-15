f9b800ebe51ae296d6daee16c397ce4279985420
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-15 02:21:42 +0200

    Resize dim col, and pass props:slug, typw

--- src/components/section/ProjectsList.vue
+++ src/components/section/ProjectsList.vue
@@ -15,18 +15,20 @@ export default {
 
 <template>
     <section id="projects_list">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
             <div class="col" v-for="p in state.projects">
                 <ProjectItem
                     :title="p.title"
                     :cover="state.completePath(p)"
-                    :overview="p.description" />
+                    :description="p.description"
+                    :type="p.type"
+                    :slug="p.slug" />
             </div>
             <!-- /.col -->
         </div>
         <!-- /.row -->
 
-        <nav class="mt-3">
+        <nav class="mt-5 mb-4">
             <ul class="pagination">
                 <li class="page-item" v-for="link in  state.links" :class="link.active ? 'active' : ''" @click="state.fetchProjects(link.url)">
                     <a class="page-link" href="#" v-html="link.label"></a>
