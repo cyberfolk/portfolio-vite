f1675c1b8f50d4078cf5a6171d9cbb590e7c74b6
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-16 15:49:02 +0200

    Add structure prototipe

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -13,16 +13,70 @@ export default {
 </script>
 
 <template>
-    <div id="view_project">
-        <div class="container" v-if="state.project">
-            <h1 class="my-5 f_skratch">{{ state.project.title }}</h1>
-            <h3>SingleProject View</h3>
+    <section id="view_project">
+        <div class="container my-5" v-if="state.project">
+            <h1 class="my-5 f_skratch">Project</h1>
+            <h1 class="align-self-center">{{ $route.params.slug }}</h1>
+            <div class="row g-5">
+                <div class="col-6">
+                    <div class="card">
+                        <div v-if="state.project" class="card">
+                            <img class="card-img-top" :src="state.completePath(state.project)" alt="Card image cap" />
+                            <div class="card-body">
+                                <h2 class="mb-2">Repo: {{ state.project.link_code }}</h2>
+                                <h3 class="mb-2">Date: {{ state.project.start_date }}</h3>
+                                <h4 class="mb-2">
+                                    Type:
+                                    <div v-if="state.project.type" class="badge">{{ state.project.type.name }}</div>
+                                    <div v-else>N/A</div>
+                                </h4>
+                                <div v-if="state.project.technologies" class="d-flex align-items-center mb-2">
+                                    <h4>Technologies:</h4>
+                                    <ul v-for="technology in state.project.technologies">
+                                        <li class="badge"> {{ technology.name }} </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card"></div>
+                </div>
+            </div>
         </div>
         <!-- /.container -->
-    </div>
+    </section>
     <!-- /#view_project -->
 </template> 
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+
+h2,
+h3,
+h4 {
+    margin: 0;
+}
+
+.card {
+    background-color: rgba($color: #e9e9e9, $alpha: 0.2);
+    border: 0;
+    color: white;
+}
+
+.badge {
+    background-color: rgb(0, 102, 255);
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    margin-left: 0.5rem;
+
+    li {
+        background-color: red !important;
+    }
+}
 </style>
