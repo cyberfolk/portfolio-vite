9ddbc2abf22859237a5851d8e4e25992a4046bae
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-16 15:48:21 +0200

    Add jumbo in main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,19 @@ export default {
 
 <template>
     <div id="view_home">
+        <section id="jumbotron" class="text-center pb-3 bg-primary">
+            <div class="d-flex justify-content-center align-items-center lh-1 font-monospace">
+                <div class="rotate_2 text_darkless" style="font-size: 150px;">&#60; </div>
+                <div class="">
+                    <h1 class="rotate_2"><span class="f_skratch text_darkless">Ciao!</span> Sono Andrea, e</h1>
+                    <h1 class="rotate_2">sono un Web<span class="f_skratch text_darkless"> Developer</span></h1>
+                </div>
+                <div class="rotate_2 text_darkless pb-5" style="font-size: 150px;">/&#62; </div>
+            </div>
+
+        </section>
         <div class="container">
-            <h1 class="my-5 f_skratch">Home View</h1>
+
         </div>
         <!-- /.container -->
     </div>
