dbc9cc53941ab8c60313305110b350ba385f8083
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-17 14:24:00 +0200

    Delete projectList & move his code to projectsview

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,22 +1,44 @@
 <script>
+import { state } from "../state";
+import ProjectItem from "./../components/item/ProjectItem.vue";
 import JumbotroSection from "./../components/section/JumbotronSection.vue";
 import SiteMain from "./../components/SiteMain.vue";
-import ProjectsList from "./../components/section/ProjectsList.vue";
 export default {
     name: "ProjectView",
     components: {
-        ProjectsList,
-        JumbotroSection,
-        SiteMain,
-    }
+        JumbotroSection, SiteMain, ProjectItem,
+    },
+    data() { return { state, }; },
+    mounted() { state.fetchProjects(state.API_URL_BASE + state.API_PAGINATION + state.currentPage); },
 };
-
 </script>
 
 <template>
     <JumbotroSection></JumbotroSection>
     <SiteMain>
-        <ProjectsList />
+        <section id="projects_list" class="mb-5 py-3">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
+                <div class="col" v-for="p in state.projects">
+                    <ProjectItem
+                        :title="p.title"
+                        :cover="state.completePath(p)"
+                        :description="p.description"
+                        :type="p.type"
+                        :technologies="p.technologies"
+                        :slug="p.slug" />
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+            <nav class="my-5 text-center">
+                <ul class="pagination justify-content-center">
+                    <li v-for="link in  state.links" class="page-item shadow" :class="link.active ? 'active' : ''" @click="state.fetchProjects(link.url)">
+                        <a class="page-link bg-dark border-0" href="#" v-html="link.label"></a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <!-- /#projects_list -->
     </SiteMain>
 </template>
 
