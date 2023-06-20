7c9bafbb86125dd8e6631f0d45c44207e6012522
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-20 23:52:01 +0200

    Import faMagnifyingGlass and faX

--- src/main.js
+++ src/main.js
@@ -9,11 +9,11 @@ import { router } from './router.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons'
 import { faXmarkCircle, faCircleXmark } from '@fortawesome/free-regular-svg-icons'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { faTelegram, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faUserSecret, faGithub, faLinkedinIn, faTelegram, faXmarkCircle, faCircleXmark, faHeart)
+library.add(faUserSecret, faGithub, faLinkedinIn, faTelegram, faXmarkCircle, faCircleXmark, faHeart, faMagnifyingGlass, faX)
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
